f90a2115453ebdba9f3c88a4f1bcf1a4c09a2646
abhis12321 <abhisheksingh2359328@gmail.com>
2024-12-06 20:08:13 +0530

    several minor changes in UI

--- src/components/BoardTile.js
+++ src/components/BoardTile.js
@@ -6,7 +6,7 @@ export default function BoardTile({ index_01, index_02 }) {
     const isActiveMoveTile = useMemo(() => isActiveMove({ rank: index_01, file: index_02 }), [index_01, index_02, activeMoves, isActiveMove]);
     return (
         <div className={`h-[calc(min(100vw,100vh)/8-1px)] aspect-square text-[10px] ${(index_01 + index_02) % 2 == 0 ? "bg-amber-100/95" : "bg-amber-700"} duration-500 flex items-center justify-center`} key={`${index_01}-${index_02}`}>
-            { isActiveMoveTile && <div className={`h-[50%] w-[50%] rounded-full bg-green-400`} />}
+            { isActiveMoveTile && <div className={`h-[40%] aspect-square rounded-full bg-black/40`} />}
         </div>
     )
 }
